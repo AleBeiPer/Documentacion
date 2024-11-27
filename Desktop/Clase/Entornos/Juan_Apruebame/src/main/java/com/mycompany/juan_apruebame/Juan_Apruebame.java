3709857ec9c90acdb81f6bd80609ceb93166ea4b
AleBeiPer <abeiper042@g.educaand.es>
2024-11-27 21:38:46 +0100

    Signed-off-by:AleBeiPer <abeiper042@g.educaand.es>

---
+++ Desktop/Clase/Entornos/Juan_Apruebame/src/main/java/com/mycompany/juan_apruebame/Juan_Apruebame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.juan_apruebame;
+
+/**
+ *
+ * @author alumno1DAW
+ */
+public class Juan_Apruebame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
