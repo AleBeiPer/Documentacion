457b6a261188ad6473cea8f0bfa2ffdaf6a4774a
AleBeiPer <abeiper042@g.educaand.es>
2024-11-27 21:39:12 +0100

    Signed-off-by:AleBeiPer <abeiper042@g.educaand.es>

--- Desktop/Clase/Entornos/Juan_Apruebame/src/main/java/com/mycompany/juan_apruebame/Juan_Apruebame.java
+++ Desktop/Clase/Entornos/Juan_Apruebame/src/main/java/com/mycompany/juan_apruebame/Juan_Apruebame.java
@@ -9,6 +9,7 @@ package com.mycompany.juan_apruebame;
  * @author alumno1DAW
  */
 public class Juan_Apruebame {
+    //Esto es una prueba
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
