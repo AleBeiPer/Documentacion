9904ec6f738b62ce881f6242e7c0f4d38a45794a
AleBeiPer <abeiper042@g.educaand.es>
2024-12-11 20:50:15 +0100

    Signed-off-by:AleBeiPer <abeiper042@g.educaand.es>

---
+++ Desktop/Clase/Entornos/documentacion/src/main/java/com/mycompany/documentacion/Documentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.documentacion;
+
+/**
+ *
+ * @author alumno1DAW
+ */
+public class Documentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
