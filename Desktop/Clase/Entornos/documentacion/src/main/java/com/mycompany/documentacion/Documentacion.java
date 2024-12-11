6886e5c2ec1367442886db90b7bc1e1dabe8dc40
AleBeiPer <abeiper042@g.educaand.es>
2024-12-11 21:11:44 +0100

    Signed-off-by:AleBeiPer <abeiper042@g.educaand.es>

--- Desktop/Clase/Entornos/documentacion/src/main/java/com/mycompany/documentacion/Documentacion.java
+++ Desktop/Clase/Entornos/documentacion/src/main/java/com/mycompany/documentacion/Documentacion.java
@@ -7,10 +7,11 @@ package com.mycompany.documentacion;
 /**
  *
  * @author alumno1DAW
+ * Cambio en el repositorio
  */
 public class Documentacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Juan!");
     }
 }
